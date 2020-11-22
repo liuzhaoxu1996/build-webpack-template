fe9f6f5de004da5e4a26933f6d2763db9b9a1500
liuzhaoxu <liuzhaoxu@360.com>
2020-11-22 13:07:03 +0800

    feat: update

--- meta.js
+++ meta.js
@@ -1,6 +1,11 @@
 
 const path = require('path')
 const fs = require('fs')
+const {
+    sortDependencies,
+    installDependencies,
+    printMessage,
+} = require('./utils')
 
 module.exports = {
     prompts: {
@@ -114,5 +119,22 @@ module.exports = {
         '.yarnrc': 'autoInstall === "yarn"',
     },
     complete: function (data, { chalk }) {
+        const green = chalk.green
+
+        sortDependencies(data, green)
+
+        const cwd = path.join(process.cwd(), data.inPlace ? '' : data.destDirName)
+
+        if (data.autoInstall) {
+            installDependencies(cwd, data.autoInstall, green)
+                .then(() => {
+                    printMessage(data, green)
+                })
+                .catch(e => {
+                    console.log(chalk.red('Error:'), e)
+                })
+        } else {
+            printMessage(data, chalk)
+        }
     },
 }
