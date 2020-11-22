2ea8226f6d55964c82babb08c56a260def077f46
liuzhaoxu <liuzhaoxu@360.com>
2020-11-22 13:31:07 +0800

    feat: update

--- utils/index.js
+++ utils/index.js
@@ -2,13 +2,6 @@ const path = require('path')
 const fs = require('fs')
 const spawn = require('child_process').spawn
 
-const lintStyles = ['standard', 'airbnb']
-
-/**
- * Sorts dependencies in package.json alphabetically.
- * They are unsorted because they were grouped for the handlebars helpers
- * @param {object} data Data from questionnaire
- */
 exports.sortDependencies = function sortDependencies(data) {
     const packageJsonFile = path.join(
         data.inPlace ? '' : data.destDirName,
@@ -20,11 +13,6 @@ exports.sortDependencies = function sortDependencies(data) {
     fs.writeFileSync(packageJsonFile, JSON.stringify(packageJson, null, 2) + '\n')
 }
 
-/**
- * Runs `npm install` in the project directory
- * @param {string} cwd Path of the created project directory
- * @param {object} data Data from questionnaire
- */
 exports.installDependencies = function installDependencies(
     cwd,
     executable = 'npm',
@@ -37,35 +25,6 @@ exports.installDependencies = function installDependencies(
     })
 }
 
-/**
- * Runs `npm run lint -- --fix` in the project directory
- * @param {string} cwd Path of the created project directory
- * @param {object} data Data from questionnaire
- */
-exports.runLintFix = function runLintFix(cwd, data, color) {
-    if (data.lint && lintStyles.indexOf(data.lintConfig) !== -1) {
-        console.log(
-            `\n\n${color(
-                'Running eslint --fix to comply with chosen preset rules...'
-            )}`
-        )
-        console.log('# ========================\n')
-        const args =
-            data.autoInstall === 'npm'
-                ? ['run', 'lint', '--', '--fix']
-                : ['run', 'lint', '--fix']
-        return runCommand(data.autoInstall, args, {
-            util
-            cwd,
-        })
-    }
-    return Promise.resolve()
-}
-
-/**
- * Prints the final message with instructions of necessary next steps.
- * @param {Object} data Data from questionnaire.
- */
 exports.printMessage = function printMessage(data, { green, yellow }) {
     const message = `
 # ${green('Project initialization finished!')}
@@ -74,44 +33,18 @@ To get started:
   ${yellow(
         `${data.inPlace ? '' : `cd ${data.destDirName}\n  `}${installMsg(
             data
-        )}${lintMsg(data)}npm run dev`
+        )}${data}npm run dev`
     )}
   
-Documentation can be found at https://sobuildjs-templates.github.io/webpack
+Documentation can be found at https://github.com/liuzhaoxu1996/sobuild-webpack-template
 `
     console.log(message)
 }
 
-/**
- * If the user will have to run lint --fix themselves, it returns a string
- * containing the instruction for this step.
- * @param {Object} data Data from questionnaire.
- */
-function lintMsg(data) {
-    return !data.autoInstall &&
-        data.lint &&
-        lintStyles.indexOf(data.lintConfig) !== -1
-        ? 'npm run lint -- --fix (or for yarn: yarn run lint --fix)\n  '
-        : ''
-}
-
-/**
- * If the user will have to run `npm install` or `yarn` themselves, it returns a string
- * containing the instruction for this step.
- * @param {Object} data Data from the questionnaire
- */
 function installMsg(data) {
     return !data.autoInstall ? 'npm install (or if using yarn: yarn)\n  ' : ''
 }
 
-/**
- * Spawns a child process and runs the specified command
- * By default, runs in the CWD and inherits stdio
- * Options are the same as node's child_process.spawn
- * @param {string} cmd
- * @param {array<string>} args
- * @param {object} options
- */
 function runCommand(cmd, args, options) {
     return new Promise((resolve, reject) => {
         const spwan = spawn(
