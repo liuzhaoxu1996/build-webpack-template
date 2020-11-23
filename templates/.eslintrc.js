ccfd9977e2dcab3f03c2ef692cac86e0093c0b74
liuzhaoxu <liuzhaoxu@360.com>
2020-11-23 18:09:29 +0800

    space to tabs

--- templates/.eslintrc.js
+++ templates/.eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-    'extends': './node_modules/@q/sofe-eslint/.eslintrc.js',
-    env: {
-        node: true,
-        es6: true,
-        mocha: true,
-    },
-    parserOptions: {
-        ecmaVersion: 8,
-    },
-    'rules': {
-        'no-console': 'off',
-    },
+	'extends': './node_modules/@q/sofe-eslint/.eslintrc.js',
+	env: {
+		node: true,
+		es6: true,
+		mocha: true,
+	},
+	parserOptions: {
+		ecmaVersion: 8,
+	},
+	'rules': {
+		'no-console': 'off',
+	},
 };
