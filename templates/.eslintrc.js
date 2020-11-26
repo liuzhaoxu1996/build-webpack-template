af9c1e650f7da1b957c19c384970968789a30952
liuzhaoxu <liuzhaoxu@360.com>
2020-11-26 10:08:57 +0800

    feat: update

--- templates/.eslintrc.js
+++ templates/.eslintrc.js
@@ -1,21 +1,18 @@
 module.exports = {
-	'extends': [
-		'./node_modules/@q/sofe-eslint/.eslintrc.js',
-	],
-	env: {
-		node: true,
-		es6: true,
-		mocha: true,
-		browser: true,
-	},
-	parserOptions: {
-		ecmaVersion: 2018,
-		sourceType: 'module',
-	},
-	rules: {
-		// eslint-disable-next-line no-negated-condition
-		'no-console': process.env.NODE_ENV !== 'production' ? 0 : 2,
-		'no-useless-escape': 0,
-		'no-empty': 0,
-	},
+    env: {
+        node: true,
+        es6: true,
+        mocha: true,
+        browser: true,
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    rules: {
+        // eslint-disable-next-line no-negated-condition
+        'no-console': process.env.NODE_ENV !== 'production' ? 0 : 2,
+        'no-useless-escape': 0,
+        'no-empty': 0,
+    },
 };
--- templates/stylelint.config.js
+++ templates/stylelint.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-	'extends': [
-		'@q/sofe-stylelint',
-		'@q/sofe-stylelint/stylelint.plugin.js',
-	],
-	'rules': {
+    'rules': {
 
-	},
+    },
 };
